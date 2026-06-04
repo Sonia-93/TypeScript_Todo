fbfe4fb1d10aa7d470d086473ebfe429f0e3df5a
Wateta <shimirwasonia5@gmail.com>
2026-06-04 09:45:09 +0200

    Initial commit

--- src/models/todoModel.ts
+++ src/models/todoModel.ts
@@ -1,10 +1,33 @@
 import { Schema,model, Document } from "mongoose";
 
-export type PriorityLevel=" 'low' | 'medium' | 'high'";
+export type PriorityLevel = 'low' | 'medium' | 'high';
 
 export interface  ITodo extends Document{
     title:string;
-    description?:string;
+    description?:string;//optional description
     isCompleted:boolean;
     priority:PriorityLevel;
 };
+
+const todoSchema= new Schema <ITodo>({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    isCompleted:{
+        type:Boolean,
+        default:false
+    },
+    priority:{
+        type:String,
+        enum:['low','medium','high'],
+        default:'medium'
+    }
+   
+})
+const Todo=model<ITodo>('Todo',todoSchema);
