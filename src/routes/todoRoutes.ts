ed32e42e91af0aa4d58fd00a0d6dacbd6ba5de63
Wateta <shimirwasonia5@gmail.com>
2026-06-04 09:51:11 +0200

    Intial commit

---
+++ src/routes/todoRoutes.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+// import {createTodo,getTodos,getTodoById,updateTodo,deleteTodo} from '../controllers/todoController';
+const router=Router();
+
+/*
+router.get('/todos',getTodos);
+router.get('/todos/:id',getTodoById);
+router.post('/todos',createTodo);
+router.put('/todos/:id',updateTodo);
+router.delete('/todos/:id',deleteTodo);
+*/
+
+export default router;
